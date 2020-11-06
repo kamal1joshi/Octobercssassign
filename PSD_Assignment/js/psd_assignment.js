df87485573c4ddda760d95e729db33db79798dd0
Master <kamal@excellencetechnologies.info>
2020-11-06 16:02:12 +0530

    PSD assignment completed

---
+++ PSD_Assignment/js/psd_assignment.js
@@ -0,0 +1,38 @@
+function myFunction() {
+  var x = document.getElementById("nav");
+  var y = document.getElementById("top_logo");
+
+  if (x.className === "col-6 col-sm-5 nav") {
+    x.className += " responsive";
+    x.classList.remove('col-6');
+    x.classList.add('col-12');
+    x.classList.add('display_show');
+    
+  } else {
+    x.className = "col-6 col-sm-5 nav";
+    x.classList.remove('col-12');
+    x.classList.remove('display_show');
+    x.classList.add('col-6');
+ 
+  }
+}
+
+function organisationTable(){
+  var x = document.getElementById("collapse_first");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+
+function individualTable(){
+  var x = document.getElementById("collapse_second");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
